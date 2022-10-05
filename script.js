aded78423fa341a3ce3b0f1632d6842c099a26f5
Archiehere <cheeku67@gmail.com>
2022-10-05 15:21:59 +0530

    slight mechanic changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let posY=250,paddir=0,prevpos=0,ballxdir=+1,ballydir=+1,ballxpos=35,ballypos=180;
+let posY=250,paddir=0,prevpos=0,ballxdir=+1,ballydir=+1,ballxpos=100,ballypos=180,score=0;
 let tem=setInterval(game,2);
 
 // var k = 0;
@@ -7,7 +7,7 @@ function game(){
     // console.log(k);
     // k++;
   document.getElementById("pad2").style.top= posY-190 + "px";
-//   document.getElementById("heading").innerHTML=-posY+640 +" " + ballypos ;
+  document.getElementById("heading").innerHTML=score ;
  
   document.getElementById("pad1").style.top= -posY+580 + "px";
   if(ballxdir==+1)
@@ -20,10 +20,26 @@ function game(){
   ballypos++;
     if(ballypos==0 || ballypos==450)
     ballydir*=-1;
-    if((ballxpos==528 && ballypos<(posY-110) && ballypos>(posY-210)))
-    ballxdir*=-1;
-    if (ballxpos==35 && ballypos<(-posY+655)&& ballypos>(-posY+555))
-    ballxdir*=-1;
+    if((ballxpos==528 && ballypos<(posY-100) && ballypos>(posY-220)))
+    {
+      score++;
+      // if(paddir==1)
+      // {ballxdir=1;}
+      // else
+      // {ballxdir=-1;}
+      ballxdir*=-1;
+    }
+    
+    if (ballxpos==35 && ballypos<(-posY+660)&& ballypos>(-posY+550))
+    {
+      score++;
+      // if(paddir==1)
+      // {ballxdir=1;}
+      // else
+      // {ballxdir=-1;}
+       ballxdir*=-1;
+    }
+     
     if(ballxpos>570 || ballxpos<2)
     {
         gameover();
